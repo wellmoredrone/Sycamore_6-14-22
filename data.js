b9e256d3bdf30673476aabd788a521f19f4278f3
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2022-06-17 08:27:03 -0400

    init

---
+++ data.js
@@ -0,0 +1,206 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-area-1",
+      "name": "Area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 0.18270364753592538,
+        "pitch": 0.33129230967787393,
+        "fov": 0.7472433115534749
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.568844608284798,
+          "pitch": -0.01883554807940868,
+          "rotation": 0,
+          "target": "1-area-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1008617570214998,
+          "pitch": 0.29717796429110876,
+          "title": "Cut-Through",
+          "text": "Going down to the Eagle A seam for June 2022."
+        },
+        {
+          "yaw": 1.458820202843044,
+          "pitch": 1.2853554430258267,
+          "title": "Spoil Fill 3",
+          "text": "Spoil Fill 3 is being filled in and will be constructed here."
+        },
+        {
+          "yaw": 1.9346197729317973,
+          "pitch": 0.223355646608713,
+          "title": "Spoil Fill 1",
+          "text": "Spoil Fill 1 is being filled in and will be constructed here."
+        }
+      ]
+    },
+    {
+      "id": "1-area-2",
+      "name": "Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 2.2134941154477605,
+        "pitch": 0.39852706407780936,
+        "fov": 0.88756600195739
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3256207252431178,
+          "pitch": 0.04432151104163573,
+          "rotation": 0,
+          "target": "0-area-1"
+        },
+        {
+          "yaw": 2.541778981329915,
+          "pitch": 0.067741383687558,
+          "rotation": 0,
+          "target": "2-area-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1604856503566943,
+          "pitch": 0.32829855942020814,
+          "title": "Auger",
+          "text": "Augering in the Clintwood A seam."
+        },
+        {
+          "yaw": -2.6237392156151564,
+          "pitch": 0.479822683526816,
+          "title": "Stockpile Area",
+          "text": "Stockpile Area"
+        }
+      ]
+    },
+    {
+      "id": "2-area-3",
+      "name": "Area 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 1.2510701715352752,
+        "pitch": 0.7390289490877944,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5799729993708382,
+          "pitch": -0.050680472042582636,
+          "rotation": 0,
+          "target": "1-area-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2827129681083562,
+          "pitch": 0.4651204128775781,
+          "title": "Auger",
+          "text": "Augering in the Clintwood A seam."
+        },
+        {
+          "yaw": 1.476412612143279,
+          "pitch": 0.5697275689717358,
+          "title": "Contour Cut",
+          "text": "Working toward the Eagle A seam."
+        },
+        {
+          "yaw": -1.3195861460231963,
+          "pitch": 0.2644915772406975,
+          "title": "Stockpile Area",
+          "text": "Stockpile Area"
+        }
+      ]
+    }
+  ],
+  "name": "Sycamore 6-14-22",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
